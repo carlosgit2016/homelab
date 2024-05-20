1e120b087b5841bbd6278a47340f427c5ea5a0c2
carlosgit2016 <carlosggflor@gmail.com>
2024-05-20 01:14:28 -0300

    Initial cluster configuration

---
+++ ansible/roles/common/tasks/files/containerd-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+# Another easy option and probably the one I should went with is to install containerd using apt-get
+
+# Containerd installation
+curl -SsL -O https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-arm64.tar.gz
+sha256sum containerd-$CONTAINERD_VERSION-linux-arm64.tar.gz
+tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-arm64.tar.gz
+
+# Systemd containerd unit configuration
+curl -SsL -O https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+mv containerd.service /lib/systemd/system/containerd.service
+systemctl daemon-reload
+systemctl enable --now containerd
+
+# Runc download and configuration
+curl -SsL -O https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.arm64
+sha256sum runc.sha256sum
+install -m 755 runc.arm64 /usr/local/sbin/runc
+
+# CNI download and configuration
+curl -SsL -O https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGIN_VERSION/cni-plugins-linux-arm64-v$CNI_PLUGIN_VERSION.tgz
+sha256sum "cni-plugins-linux-arm64-v$CNI_PLUGIN_VERSION.tgz"
+if [ ! -e /opt/cni/bin ]; then
+    mkdir /opt/cni/bin
+fi
+tar Cxzvf /opt/cni/bin cni-plugins-linux-arm64-v$CNI_PLUGIN_VERSION.tgz
+
+# Generating containerd initial config
+if [ ! -e /etc/containerd ]; then
+    mkdir /etc/containerd
+fi
+containerd config default > /etc/containerd/config.toml
+
+# Setting to use systemd cgroup driver
+sed -i "s|SystemdCgroup\ =\ false|SystemdCgroup\ =\ true|" /etc/containerd/config.toml
+systemctl restart containerd
