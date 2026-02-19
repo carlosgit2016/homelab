aa5ea2485e4a5ba5dd23131bf0be55b4580032a8
carlosgit2016 <gabrielggff25@gmail.com>
2026-02-18 21:22:29 -0300

    chore(ansible): upgrade kubernetes 1.35 and runtime versions

--- ansible/roles/containerd/tasks/files/containerd-install.sh
+++ ansible/roles/containerd/tasks/files/containerd-install.sh
@@ -4,12 +4,31 @@ set -e
 
 # Another easy option and probably the one I should went with is to install containerd using apt-get
 
+# Detect architecture
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64)
+        CONTAINERD_ARCH="amd64"
+        RUNC_ARCH="amd64"
+        CNI_ARCH="amd64"
+        ;;
+    aarch64)
+        CONTAINERD_ARCH="arm64"
+        RUNC_ARCH="arm64"
+        CNI_ARCH="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
 pushd /root/
 
 # Containerd installation
-curl -SsL -O https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-arm64.tar.gz
-sha256sum containerd-$CONTAINERD_VERSION-linux-arm64.tar.gz
-tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-arm64.tar.gz
+curl -SsL -O https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-$CONTAINERD_ARCH.tar.gz
+sha256sum containerd-$CONTAINERD_VERSION-linux-$CONTAINERD_ARCH.tar.gz
+tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-$CONTAINERD_ARCH.tar.gz
 
 # Systemd containerd unit configuration
 curl -SsL -O https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
@@ -18,17 +37,17 @@ systemctl daemon-reload
 systemctl enable --now containerd
 
 # Runc download and configuration
-curl -SsL -O https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.arm64
+curl -SsL -O https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.$RUNC_ARCH
 sha256sum runc.sha256sum
-install -m 755 runc.arm64 /usr/local/sbin/runc
+install -m 755 runc.$RUNC_ARCH /usr/local/sbin/runc
 
 # CNI download and configuration
-curl -SsL -O https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGIN_VERSION/cni-plugins-linux-arm64-v$CNI_PLUGIN_VERSION.tgz
-sha256sum "cni-plugins-linux-arm64-v$CNI_PLUGIN_VERSION.tgz"
+curl -SsL -O https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGIN_VERSION/cni-plugins-linux-$CNI_ARCH-v$CNI_PLUGIN_VERSION.tgz
+sha256sum "cni-plugins-linux-$CNI_ARCH-v$CNI_PLUGIN_VERSION.tgz"
 if [ ! -e /opt/cni/bin ]; then
     mkdir -p /opt/cni/bin
 fi
-tar Cxzvf /opt/cni/bin cni-plugins-linux-arm64-v$CNI_PLUGIN_VERSION.tgz
+tar Cxzvf /opt/cni/bin cni-plugins-linux-$CNI_ARCH-v$CNI_PLUGIN_VERSION.tgz
 
 # Generating containerd initial config
 if [ ! -e /etc/containerd ]; then
